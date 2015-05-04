58646ac761279e6639506ae6b3501f87a34b7ed5
Casper <caspernaruto@qip.ru>
2015-05-04 16:40:09 +0300

    footer, ewe chet ewe chet flexbox

--- resources/views/layouts/inc/header.blade.php
+++ resources/views/layouts/inc/header.blade.php
@@ -1,14 +1,14 @@
 <div class="header">
 	<div class="header-title">PRIVET</div>
 	<ul class="header-navbar">
-		<a class='header-item' href='/'>HOME</a>
-		<li class="header-item">TEMPLATES
+		<a class='header-item' href='/'><div class="header-item-name">HOME</div></a>
+		<li class="header-item"> <div class='header-item-name'>TEMPLATES</div>
 			<ul class="header-item-drop">
 				<li><a href='./default' class='pages'>DEFAULT PAGE</a></li>
 				<li><a href='./fullwidth' class="pages">FULL-WIDTH PAGE</a></li>
 				<li><a href='./contact' class='pages'>CONTACT PAGE</a></li>
 			</ul>
 		</li>
-		<a class='header-item' href='./contact'>CONTACT</a>
+		<a class='header-item' href='./contact'><div class="header-item-name">CONTACT</div></a>
 	</ul>
 </div>
